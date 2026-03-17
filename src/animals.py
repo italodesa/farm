c6e177ef2e9e3259911c275138a5fab9683492ad
italodesa <italodesa0101@gmail.com>
2026-03-16 21:09:52 -0300

    feat: add Animal class

---
+++ src/animals.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self,specie, age, weight, status,animal_id = None):
+        self.id = animal_id
+        self.specie = specie
+        self.age = age
+        self.weight = weight
+        self.status = status
+
