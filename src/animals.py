c23da8ca4aab6a0e0f695af3f1db6e6de30e4927
italodesa <italodesa0101@gmail.com>
2026-03-17 14:10:32 -0300

    feat: add create_animal class method for instance creation

--- src/animals.py
+++ src/animals.py
@@ -1,8 +1,24 @@
+from utils import generate_id,write_file
 class Animal:
     def __init__(self,specie, age, weight, status,animal_id = None):
-        self.id = animal_id
+        self.animal_id = animal_id
         self.specie = specie
         self.age = age
         self.weight = weight
         self.status = status
 
+    @classmethod
+    def create_animal(cls):
+        while True:
+            try:
+                specie = input("Digite a especie do animal: ")
+                age = int(input("Digite a idade do animal: "))
+                weight = float(input("Digite o peso do animal: "))
+                status = input("Digite o status do animal: ")
+                animal_id = generate_id("animals.json","animal_id")
+                break
+            except ValueError:
+                print("Entrada inválida. Por favor, tente novamente.")
+        
+        animal = cls(specie, age, weight, status, animal_id)
+        write_file("animals.json", animal.__dict__)
