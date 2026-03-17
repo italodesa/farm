4d8e58d068c9fc0fc408b90cce0bce76ff5b3753
italodesa <italodesa0101@gmail.com>
2026-03-16 21:51:28 -0300

    feat(utils): add function verify to ensure data directory exists and resolve file paths

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,23 @@
 
+from pathlib import Path
+import json
+
+
+def verify(path):
+# Função para verificação de arquivos
+
+        # variaveis que vão receber os caminhos dos arquivos
+        BASE_DIR = Path(path).resolve().parent.parent
+        DATA_DIR = BASE_DIR / "data"
+        DATA_FILE = DATA_DIR / path
+
+        #criar a pasta data caso ela não exista
+        DATA_DIR.mkdir(exist_ok=True)
+
+        #criar o arquivo caso ele não exista na pasta
+        if not DATA_FILE.exists():
+            with open(DATA_FILE, "w", encoding="utf-8") as f:
+                   json.dump([], f)
+        
+        #retorna o caminho do arquivo
+        return DATA_FILE
