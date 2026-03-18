70623a8e468a70100831c8caef1396d1b0d265ad
italodesa <italodesa0101@gmail.com>
2026-03-18 16:03:45 -0300

    feat: add class method create_input_object

--- src/inputs.py
+++ src/inputs.py
@@ -1,3 +1,5 @@
+from files import *
+
 class Input:
     def __init__(self,name,quantity,unit,category,input_id = None):
         self.input_id = input_id
@@ -5,3 +7,21 @@ class Input:
         self.quantity = quantity
         self.unit = unit
         self.category = category
+
+    @classmethod
+    def create_input_object(cls):
+        while True:
+            try:
+                name = input("Digite o nome do insumo: ")
+                quantity = float(input("Digite a quantidade do insumo: "))
+                unit = input("Digite a unidade de medida do insumo: ")
+                category = input("Digite a categoria do insumo "
+                "(ex: feed, fertilizer, seed, etc.): ")
+                input_id = generate_id("inputs.json", "input_id")
+                break
+            except ValueError:
+                print("Entrada inválida. Por favor, tente novamente.")
+        input_object = cls(name, quantity, unit, category, input_id)
+        write_file("inputs.json", input_object.__dict__)
+
+Input.create_input_object()
